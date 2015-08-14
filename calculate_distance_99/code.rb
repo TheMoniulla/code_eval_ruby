9aae5498a73588a7261e5f941baccbe250a56b26
Michal Czyz <michalczyz@gmail.com>
2015-08-14 15:16:14 +0200

    refactoring - ood approach

--- calculate_distance_99/code.rb
+++ calculate_distance_99/code.rb
@@ -1,10 +1,20 @@
+class DistanceCalculation < Struct.new(:x1, :y1, :x2, :y2)
+  def distance
+    Math.sqrt((vector_x**2) + (vector_y**2)).to_i
+  end
+
+  private
+
+  def vector_x
+    x1 - x2
+  end
+
+  def vector_y
+    y1 - y2
+  end
+end
+
 File.open('input.txt').each_line do |line|
-  numbers = line.scan(/-?\d+/)
-
-  x1, y1, x2, y2 = *numbers.map! {|number| number.to_i }
-
-  vector_x = x1 - x2
-  vector_y = y1 - y2
-
-  puts Math.sqrt((vector_x**2) + (vector_y**2)).to_i
+  input = line.scan(/-?\d+/).map(&:to_i)
+  puts DistanceCalculation.new(*input).distance
 end
